7b9fc1285dd28f55a30252e8b7feb74b7e3d9b9b
nicolgit <nicola.delfino@outlook.com>
2023-11-15 13:23:12 +0100

    base.dart

--- code/app/blastapp/lib/ViewModel/splash_view_model.dart
+++ code/app/blastapp/lib/ViewModel/splash_view_model.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
+import 'package:blastapp/ViewModel/base.dart';
 import 'package:blastapp/blast_router.dart';
 import 'package:blastmodel/settings_service.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewModel extends ChangeNotifier{
+class SplashViewModel extends BaseViewModel{
   BuildContext context;
 
   SplashViewModel(this.context);
@@ -14,5 +17,10 @@ class SplashViewModel extends ChangeNotifier{
 
   showEula() async {
     return context.router.push(const EulaRoute());
+  }
+  
+  @override
+  FutureOr<void> init() {
+
   }
 }

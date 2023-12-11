525001b29885454ab168f8bcce7841e89edf13d4
nicolgit <nicola.delfino@outlook.com>
2023-12-11 18:36:02 +0100

    blastdocument

--- code/app/blastmodel/lib/Cloud/fake_cloud.dart
+++ code/app/blastmodel/lib/Cloud/fake_cloud.dart
@@ -4,10 +4,22 @@ import 'dart:typed_data';
 
 import 'package:blastmodel/Cloud/cloud.dart';
 import 'package:blastmodel/Cloud/cloud_object.dart';
+import 'package:blastmodel/blastattribute.dart';
+import 'package:blastmodel/blastattributetype.dart';
+import 'package:blastmodel/blastcard.dart';
+import 'package:blastmodel/blastdocument.dart';
 
 class FakeCloud extends Cloud {
+
+  final _source = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+  late List<String> _words;
+
+  FakeCloud() {
+    _words = _source.split(" ");
+  }
+
   @override
-  String get name => 'Fake Cloud - for testing purposes only';
+  String get name => 'Fake Cloud(tm) - for testing purposes only';
 
   @override
   Future<List<CloudObject>> getFiles(String path) {
@@ -41,17 +53,80 @@ class FakeCloud extends Cloud {
 
   @override
   Uint8List getFile(String path) {
-    String input = 'Hello, World!';
-    
-    Uint8List byteArray = Uint8List.fromList(utf8.encode(input))
+    BlastDocument document = buildRandomBlastDocument();
+
+    String input = "this is a test";
+
+    Uint8List byteArray = Uint8List.fromList(utf8.encode(input));
 
     return byteArray;
   }
 
-  String buildRandomBlastDocument()
+  BlastDocument buildRandomBlastDocument()
   {
-    // TODO
-    throw UnimplementedError();
+    Random random = Random();
+
+    BlastDocument document = BlastDocument();
+    
+    int totalCards = random.nextInt(50);
+
+    for (int i=0; i<totalCards; i++)
+    {
+      BlastCard card = BlastCard();
+ 
+      card.title = _randomStringGenerator(random.nextInt(5));
+      card.notes = _randomStringGenerator(random.nextInt(20));
+      card.isFavorite = random.nextInt(10) == 0;
+      card.lastUpdateDateTime = DateTime.now().subtract(Duration(days: random.nextInt(365)));
+      card.lastOpenedDateTime = DateTime.now().subtract(Duration(days: random.nextInt(365)));
+      card.usedCounter = random.nextInt(100);
+      card.tags = _randomTagsGenerator();
+
+      int totalCards = random.nextInt(100);
+      for (int i=0; i<totalCards; i++)
+      {
+        BlastAttribute attribute = BlastAttribute();
+        attribute.name = _randomStringGenerator(random.nextInt(4));
+        attribute.value = _randomStringGenerator(random.nextInt(10));
+        attribute.type = BlastAttributeType.values[random.nextInt(BlastAttributeType.values.length)];
+
+        card.rows.add(attribute);
+      } 
+
+      document.cards.add(card);
+    }
+
+    return document;
   }
-  
+
+
+  String _randomStringGenerator(int length)
+  {
+    Random random = Random();
+
+    String result = "";
+
+    for (int i=0; i<length; i++)
+    {
+      result += _words[random.nextInt(_words.length)];
+      result += " ";
+    }
+
+    return result;
+  } 
+
+  List<String> _randomTagsGenerator()
+  {
+    Random random = Random();
+
+    List<String> result = [];
+
+    int length = random.nextInt(3);
+    for (int i=0; i<length; i++)
+    {
+      result.add(_words[random.nextInt(_words.length)]);
+    }
+
+    return result;
+  }
 }
--- code/app/blastmodel/lib/blastdocument.dart
+++ code/app/blastmodel/lib/blastdocument.dart
@@ -2,22 +2,10 @@ import 'package:blastmodel/blastcard.dart';
 import 'package:uuid/uuid.dart';
 
 class BlastDocument {
-  BlastDocument(); /* {
-    Id = const Uuid().v4();
-    Version = 10;
-    Cards = List<BlastCard>.empty(growable: true);
-
-    var card1 = BlastCard();
-    card1.Title = "lorem ipsut dixit";
-
-    var card2 = BlastCard();
-    card2.Title = "consectetur adipiscing elit";
-
-    Cards.add(card1);
-    Cards.add(card2);
-  }*/
+  BlastDocument(); 
 
   String id = const Uuid().v4();
-  int Version = 1;
-  List<BlastCard> Cards = List<BlastCard>.empty(growable: true);
+  int version = 1;
+
+  List<BlastCard> cards = List<BlastCard>.empty(growable: true);
 }

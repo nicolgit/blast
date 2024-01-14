e2b6f6c00001aa49e7d0c77d9ae01ef4130c34d8
NicolD <nicola.delfino@outlook.com>
2024-01-14 17:06:26 +0100

    working on filsesystem cloud

--- code/app/blastmodel/lib/Cloud/filesystem_cloud.dart
+++ code/app/blastmodel/lib/Cloud/filesystem_cloud.dart
@@ -1,16 +1,28 @@
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:blastmodel/Cloud/cloud.dart';
 import 'package:blastmodel/Cloud/cloud_object.dart';
+import 'package:path_provider/path_provider.dart';
 
 class FileSystemCloud extends Cloud {
   @override
   String get name => 'local file system';
 
   @override
-  Future<List<CloudObject>> getFiles(String path) {
-    // TODO: implement getFiles
-    throw UnimplementedError();
+  Future<List<CloudObject>> getFiles(String path) async {
+    Directory appDocumentsDir = await getApplicationDocumentsDirectory();
+
+    List<CloudObject> files = [];
+    files.add(CloudObject(
+        name: 'test',
+        path: appDocumentsDir.path,
+        size: 0,
+        url: appDocumentsDir.path,
+        lastModified: DateTime.now(),
+        isDirectory: true));
+
+    return files;
   }
 
   @override

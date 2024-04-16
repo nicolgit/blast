ad61c5bbeb655b2c340585573ddff982adaca1b6
NicolD <nicola.delfino@outlook.com>
2024-04-16 14:41:57 +0200

    removed sample cards

--- code/pazword-converter/DataFile/BlastDocument.cs
+++ code/pazword-converter/DataFile/BlastDocument.cs
@@ -11,15 +11,6 @@ namespace Blast.Model.DataFile
             Id = Guid.NewGuid();
             Version = 10;
             Cards = new List<Card>();
-
-            var card1 = new Card();
-            card1.Title = "lorem ipsut dixit";
-
-            var card2 = new Card();
-            card1.Title = "consectetur adipiscing elit";
-
-            Cards.Add(card1);
-            Cards.Add(card2);
         }
 
         public Guid Id { get; set; }

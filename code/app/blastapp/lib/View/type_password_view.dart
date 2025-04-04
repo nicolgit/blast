f6acc812b443b56d5cb1fc4e4d95b47e79d3598d
Nicola Delfino <nicolgit@users.noreply.github.com>
2025-04-04 14:24:39 +0200

    scroll missing

--- code/app/blastapp/lib/View/type_password_view.dart
+++ code/app/blastapp/lib/View/type_password_view.dart
@@ -66,111 +66,113 @@ class _TypePasswordViewState extends State<TypePasswordView> {
         color: _theme.colorScheme.surface,
         child: SafeArea(
             child: Scaffold(
-          backgroundColor: _theme.colorScheme.surface,
-          body: Center(
-              child: Column(
-            children: [
-              AppBar(
-                title: Text("file: ${vm.fileName}"),
-              ),
-              const SizedBox(height: 12.0),
-              Image.asset(vm.cloudIcon, width: 128, height: 128),
-              Text('type the master password or the recovery key to continue', style: _textTheme.labelMedium),
-              const SizedBox(height: 12.0),
-              FutureBuilder<bool>(
-                future: vm.isPasswordValid(),
-                builder: (context, isPasswordValid) {
-                  return Icon(isPasswordValid.data ?? false ? Icons.lock_open : Icons.lock,
-                      color: _theme.colorScheme.primary, size: 48.0);
-                },
-              ),
-              const SizedBox(height: 12.0),
-              SegmentedButton<PasswordType>(
-                  segments: <ButtonSegment<PasswordType>>[
-                    ButtonSegment<PasswordType>(
-                      label: Text('Password', style: _textTheme.labelSmall),
-                      value: PasswordType.password,
-                    ),
-                    ButtonSegment<PasswordType>(
-                      label: Text('Recovery Key', style: _textTheme.labelSmall),
-                      value: PasswordType.recoveryKey,
-                    ),
-                  ],
-                  selected: <PasswordType>{
-                    vm.passwordType
-                  },
-                  onSelectionChanged: (Set<PasswordType> newSelection) {
-                    setState(() {
-                      vm.passwordType = newSelection.first;
-                    });
-                  }),
-              FutureBuilder<PasswordType>(
-                future: vm.getPasswordType(),
-                builder: (context, passwordType) {
-                  if (passwordType.data == PasswordType.password) {
-                    return Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Container(
-                            constraints: BoxConstraints(maxWidth: 600),
-                            child: TextField(
-                                autofocus: true,
-                                focusNode: passwordFocusNode,
-                                obscureText: true,
-                                controller: passwordController,
-                                onChanged: (value) => vm.setPassword(value),
-                                onSubmitted: (value) async => {
-                                      if (!await vm.checkPassword(true)) passwordFocusNode.requestFocus(),
-                                    },
-                                style: _textTheme.labelMedium,
-                                decoration: InputDecoration(
-                                    border: const OutlineInputBorder(),
-                                    labelText: 'Password',
-                                    hintText: 'Enter your password',
-                                    hintStyle: _textThemeHint))));
-                  } else {
-                    return Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: TextField(
-                          onChanged: (value) => vm.setRecoveryKey(maskRecoveryKeyFormatter.getUnmaskedText()),
-                          onSubmitted: (value) async => {
-                            vm.setRecoveryKey(maskRecoveryKeyFormatter.getUnmaskedText()),
-                            if (!await vm.checkPassword(false)) passwordFocusNode.requestFocus(),
+                backgroundColor: _theme.colorScheme.surface,
+                body: SingleChildScrollView(
+                  child: Center(
+                      child: Column(
+                    children: [
+                      AppBar(
+                        title: Text("file: ${vm.fileName}"),
+                      ),
+                      const SizedBox(height: 12.0),
+                      Image.asset(vm.cloudIcon, width: 128, height: 128),
+                      Text('type the master password or the recovery key to continue', style: _textTheme.labelMedium),
+                      const SizedBox(height: 12.0),
+                      FutureBuilder<bool>(
+                        future: vm.isPasswordValid(),
+                        builder: (context, isPasswordValid) {
+                          return Icon(isPasswordValid.data ?? false ? Icons.lock_open : Icons.lock,
+                              color: _theme.colorScheme.primary, size: 48.0);
+                        },
+                      ),
+                      const SizedBox(height: 12.0),
+                      SegmentedButton<PasswordType>(
+                          segments: <ButtonSegment<PasswordType>>[
+                            ButtonSegment<PasswordType>(
+                              label: Text('Password', style: _textTheme.labelSmall),
+                              value: PasswordType.password,
+                            ),
+                            ButtonSegment<PasswordType>(
+                              label: Text('Recovery Key', style: _textTheme.labelSmall),
+                              value: PasswordType.recoveryKey,
+                            ),
+                          ],
+                          selected: <PasswordType>{
+                            vm.passwordType
                           },
-                          style: _textTheme.labelMedium,
-                          inputFormatters: [maskRecoveryKeyFormatter],
-                          decoration: InputDecoration(
-                              border: const OutlineInputBorder(),
-                              labelText: 'recovery key',
-                              hintText: '12345678-12345678-12345678-12345678-12345678-12345678-12345678-12345678',
-                              hintStyle: _textThemeHint),
-                        ));
-                  }
-                },
-              ),
-              Text(vm.errorMessage, style: _textThemeError),
-              const SizedBox(height: 12.0),
-              FutureBuilder<bool>(
-                future: vm.isPasswordValid(),
-                builder: (context, isPasswordValid) {
-                  return FilledButton(
-                    onPressed: isPasswordValid.data ?? false ? () => vm.checkPassword(true) : null,
-                    child: const Text('open'),
-                  );
-                },
-              ),
-              const SizedBox(height: 12.0),
-              FutureBuilder<bool>(
-                future: vm.isCheckingPassword(),
-                builder: (context, isCheckingPassword) {
-                  if (isCheckingPassword.data ?? false) {
-                    return CircularProgressIndicator(); //Text("checking password...", style: _textTheme.labelMedium);
-                  } else {
-                    return Text("", style: _textTheme.labelMedium);
-                  }
-                },
-              ),
-            ],
-          )),
-        )));
+                          onSelectionChanged: (Set<PasswordType> newSelection) {
+                            setState(() {
+                              vm.passwordType = newSelection.first;
+                            });
+                          }),
+                      FutureBuilder<PasswordType>(
+                        future: vm.getPasswordType(),
+                        builder: (context, passwordType) {
+                          if (passwordType.data == PasswordType.password) {
+                            return Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Container(
+                                    constraints: BoxConstraints(maxWidth: 600),
+                                    child: TextField(
+                                        autofocus: true,
+                                        focusNode: passwordFocusNode,
+                                        obscureText: true,
+                                        controller: passwordController,
+                                        onChanged: (value) => vm.setPassword(value),
+                                        onSubmitted: (value) async => {
+                                              if (!await vm.checkPassword(true)) passwordFocusNode.requestFocus(),
+                                            },
+                                        style: _textTheme.labelMedium,
+                                        decoration: InputDecoration(
+                                            border: const OutlineInputBorder(),
+                                            labelText: 'Password',
+                                            hintText: 'Enter your password',
+                                            hintStyle: _textThemeHint))));
+                          } else {
+                            return Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: TextField(
+                                  onChanged: (value) => vm.setRecoveryKey(maskRecoveryKeyFormatter.getUnmaskedText()),
+                                  onSubmitted: (value) async => {
+                                    vm.setRecoveryKey(maskRecoveryKeyFormatter.getUnmaskedText()),
+                                    if (!await vm.checkPassword(false)) passwordFocusNode.requestFocus(),
+                                  },
+                                  style: _textTheme.labelMedium,
+                                  inputFormatters: [maskRecoveryKeyFormatter],
+                                  decoration: InputDecoration(
+                                      border: const OutlineInputBorder(),
+                                      labelText: 'recovery key',
+                                      hintText:
+                                          '12345678-12345678-12345678-12345678-12345678-12345678-12345678-12345678',
+                                      hintStyle: _textThemeHint),
+                                ));
+                          }
+                        },
+                      ),
+                      Text(vm.errorMessage, style: _textThemeError),
+                      const SizedBox(height: 12.0),
+                      FutureBuilder<bool>(
+                        future: vm.isPasswordValid(),
+                        builder: (context, isPasswordValid) {
+                          return FilledButton(
+                            onPressed: isPasswordValid.data ?? false ? () => vm.checkPassword(true) : null,
+                            child: const Text('open'),
+                          );
+                        },
+                      ),
+                      const SizedBox(height: 12.0),
+                      FutureBuilder<bool>(
+                        future: vm.isCheckingPassword(),
+                        builder: (context, isCheckingPassword) {
+                          if (isCheckingPassword.data ?? false) {
+                            return CircularProgressIndicator(); //Text("checking password...", style: _textTheme.labelMedium);
+                          } else {
+                            return Text("", style: _textTheme.labelMedium);
+                          }
+                        },
+                      ),
+                    ],
+                  )),
+                ))));
   }
 }

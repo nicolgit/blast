3908d2076ba4abc0911ee29bcc0a9e5bee97e631
Nicola Delfino <nicolgit@users.noreply.github.com>
2025-11-17 17:29:47 +0100

    better url in lorem cloud

--- code/app/blastmodel/lib/Cloud/lorem_cloud.dart
+++ code/app/blastmodel/lib/Cloud/lorem_cloud.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:math';
 import 'dart:typed_data';
 
@@ -9,13 +10,138 @@ import 'package:blastmodel/blastcard.dart';
 import 'package:blastmodel/blastdocument.dart';
 import 'package:blastmodel/currentfile_service.dart';
 
+class _Website {
+  final String name;
+  final String url;
+
+  _Website({required this.name, required this.url});
+
+  factory _Website.fromJson(Map<String, dynamic> json) {
+    return _Website(
+      name: json['name'] as String,
+      url: json['url'] as String,
+    );
+  }
+}
+
 class LoremCloud extends Cloud {
   final _source =
       "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+  final _popularWebsitesJson = '''[
+  {"name": "Google", "url": "https://www.google.com"},
+  {"name": "YouTube", "url": "https://www.youtube.com"},
+  {"name": "Facebook", "url": "https://www.facebook.com"},
+  {"name": "Wikipedia", "url": "https://www.wikipedia.org"},
+  {"name": "Instagram", "url": "https://www.instagram.com"},
+  {"name": "Bing", "url": "https://www.bing.com"},
+  {"name": "Reddit", "url": "https://www.reddit.com"},
+  {"name": "X (Twitter)", "url": "https://www.x.com"},
+  {"name": "ChatGPT", "url": "https://www.chatgpt.com"},
+  {"name": "Yandex", "url": "https://www.yandex.ru"},
+  {"name": "WhatsApp", "url": "https://www.whatsapp.com"},
+  {"name": "Amazon", "url": "https://www.amazon.com"},
+  {"name": "Yahoo", "url": "https://www.yahoo.com"},
+  {"name": "Yahoo Japan", "url": "https://www.yahoo.co.jp"},
+  {"name": "Weather", "url": "https://www.weather.com"},
+  {"name": "DuckDuckGo", "url": "https://www.duckduckgo.com"},
+  {"name": "TikTok", "url": "https://www.tiktok.com"},
+  {"name": "Temu", "url": "https://www.temu.com"},
+  {"name": "Naver", "url": "https://www.naver.com"},
+  {"name": "Microsoft Online", "url": "https://www.microsoftonline.com"},
+  {"name": "Twitch", "url": "https://www.twitch.tv"},
+  {"name": "Twitter", "url": "https://www.twitter.com"},
+  {"name": "LinkedIn", "url": "https://www.linkedin.com"},
+  {"name": "Live", "url": "https://www.live.com"},
+  {"name": "Fandom", "url": "https://www.fandom.com"},
+  {"name": "Microsoft", "url": "https://www.microsoft.com"},
+  {"name": "MSN", "url": "https://www.msn.com"},
+  {"name": "Netflix", "url": "https://www.netflix.com"},
+  {"name": "Office", "url": "https://www.office.com"},
+  {"name": "Pinterest", "url": "https://www.pinterest.com"},
+  {"name": "Mail.ru", "url": "https://www.mail.ru"},
+  {"name": "OpenAI", "url": "https://www.openai.com"},
+  {"name": "AliExpress", "url": "https://www.aliexpress.com"},
+  {"name": "PayPal", "url": "https://www.paypal.com"},
+  {"name": "VK", "url": "https://www.vk.com"},
+  {"name": "Canva", "url": "https://www.canva.com"},
+  {"name": "GitHub", "url": "https://www.github.com"},
+  {"name": "Spotify", "url": "https://www.spotify.com"},
+  {"name": "Discord", "url": "https://www.discord.com"},
+  {"name": "Apple", "url": "https://www.apple.com"},
+  {"name": "IMDb", "url": "https://www.imdb.com"},
+  {"name": "Globo", "url": "https://www.globo.com"},
+  {"name": "Roblox", "url": "https://www.roblox.com"},
+  {"name": "Amazon Japan", "url": "https://www.amazon.co.jp"},
+  {"name": "Quora", "url": "https://www.quora.com"},
+  {"name": "Bilibili", "url": "https://www.bilibili.com"},
+  {"name": "Samsung", "url": "https://www.samsung.com"},
+  {"name": "eBay", "url": "https://www.ebay.com"},
+  {"name": "NY Times", "url": "https://www.nytimes.com"},
+  {"name": "Walmart", "url": "https://www.walmart.com"},
+  {"name": "Amazon Germany", "url": "https://www.amazon.de"},
+  {"name": "ESPN", "url": "https://www.espn.com"},
+  {"name": "Dailymotion", "url": "https://www.dailymotion.com"},
+  {"name": "Google Brazil", "url": "https://www.google.com.br"},
+  {"name": "BBC", "url": "https://www.bbc.com"},
+  {"name": "Rakuten Japan", "url": "https://www.rakuten.co.jp"},
+  {"name": "BBC UK", "url": "https://www.bbc.co.uk"},
+  {"name": "Telegram", "url": "https://www.telegram.org"},
+  {"name": "Indeed", "url": "https://www.indeed.com"},
+  {"name": "CNN", "url": "https://www.cnn.com"},
+  {"name": "Dzen", "url": "https://www.dzen.ru"},
+  {"name": "Booking", "url": "https://www.booking.com"},
+  {"name": "Google UK", "url": "https://www.google.co.uk"},
+  {"name": "USPS", "url": "https://www.usps.com"},
+  {"name": "Zoom", "url": "https://www.zoom.us"},
+  {"name": "Amazon UK", "url": "https://www.amazon.co.uk"},
+  {"name": "Adobe", "url": "https://www.adobe.com"},
+  {"name": "UOL", "url": "https://www.uol.com.br"},
+  {"name": "Etsy", "url": "https://www.etsy.com"},
+  {"name": "Steam", "url": "https://store.steampowered.com"},
+  {"name": "Marca", "url": "https://www.marca.com"},
+  {"name": "Ozon", "url": "https://www.ozon.ru"},
+  {"name": "Cricbuzz", "url": "https://www.cricbuzz.com"},
+  {"name": "Prime Video", "url": "https://www.primevideo.com"},
+  {"name": "Google Germany", "url": "https://www.google.de"},
+  {"name": "AccuWeather", "url": "https://www.accuweather.com"},
+  {"name": "Rutube", "url": "https://www.rutube.ru"},
+  {"name": "Instructure", "url": "https://www.instructure.com"},
+  {"name": "Disney+", "url": "https://www.disneyplus.com"},
+  {"name": "The Guardian", "url": "https://www.theguardian.com"},
+  {"name": "Infobae", "url": "https://www.infobae.com"},
+  {"name": "Google Spain", "url": "https://www.google.es"},
+  {"name": "Google Italy", "url": "https://www.google.it"},
+  {"name": "Ya.ru", "url": "https://www.ya.ru"},
+  {"name": "Ecosia", "url": "https://www.ecosia.org"},
+  {"name": "Daily Mail", "url": "https://www.dailymail.co.uk"},
+  {"name": "Amazon India", "url": "https://www.amazon.in"},
+  {"name": "Google Japan", "url": "https://www.google.co.jp"},
+  {"name": "Amazon Italy", "url": "https://www.amazon.it"},
+  {"name": "Google France", "url": "https://www.google.fr"},
+  {"name": "IKEA", "url": "https://www.ikea.com"},
+  {"name": "Google India", "url": "https://www.google.co.in"},
+  {"name": "Fox News", "url": "https://www.foxnews.com"},
+  {"name": "Amazon France", "url": "https://www.amazon.fr"},
+  {"name": "Max", "url": "https://www.max.com"},
+  {"name": "Google Canada", "url": "https://www.google.ca"},
+  {"name": "ILovePDF", "url": "https://www.ilovepdf.com"},
+  {"name": "Avito", "url": "https://www.avito.ru"},
+  {"name": "Outlook", "url": "https://www.outlook.com"},
+  {"name": "Wildberries", "url": "https://www.wildberries.ru"}
+]''';
+
   late List<String> _words;
+  late List<_Website> _websites;
 
   LoremCloud() {
     _words = _source.split(" ");
+    _loadWebsites();
+  }
+
+  void _loadWebsites() {
+    final List<dynamic> jsonData = jsonDecode(_popularWebsitesJson);
+    _websites = jsonData.map((json) => _Website.fromJson(json)).toList();
   }
 
   @override
@@ -104,7 +230,7 @@ class LoremCloud extends Cloud {
 
     BlastDocument document = BlastDocument();
 
-    int totalCards = random.nextInt(50);
+    int totalCards = random.nextInt(150);
 
     for (int i = 0; i < totalCards; i++) {
       BlastCard card = BlastCard();
@@ -113,12 +239,11 @@ class LoremCloud extends Cloud {
       card.notes = _randomStringGenerator(random.nextInt(200) + 1, true, true);
       card.isFavorite = random.nextInt(5) == 0;
       card.lastUpdateDateTime = DateTime.now().subtract(Duration(days: random.nextInt(365)));
-      //card.lastOpenedDateTime = DateTime.now().subtract(Duration(days: random.nextInt(365)));
       card.usedCounter = random.nextInt(100);
       card.tags = _randomTagsGenerator(random.nextInt(5));
 
-      int totalCards = random.nextInt(100);
-      for (int i = 0; i < totalCards; i++) {
+      int totalAttributes = random.nextInt(15);
+      for (int i = 0; i < totalAttributes; i++) {
         BlastAttribute attribute = BlastAttribute();
         attribute.name = _randomStringGenerator(random.nextInt(4) + 1, false, false);
         attribute.type = BlastAttributeType.values[random.nextInt(BlastAttributeType.values.length)];
@@ -171,7 +296,7 @@ class LoremCloud extends Cloud {
 
   String _randomUrlGenerator() {
     Random random = Random();
-    return "http://loremcloud.com/${_randomStringGenerator(random.nextInt(4), false, false).replaceAll(' ', '/')}";
+    return _websites[random.nextInt(_websites.length)].url;
   }
 
   List<String> _randomTagsGenerator(int tagsCount) {
